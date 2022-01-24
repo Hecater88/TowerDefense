91ba3e9ad434df232b01d50ffb37c1fbdc7b3ae0
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2022-01-24 23:11:43 +0100

    Enemy1 fix

--- TowerDefense/Assets/Scripts/EnemyLookAt.cs
+++ TowerDefense/Assets/Scripts/EnemyLookAt.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyLookAt : MonoBehaviour
 {
+    Camera mainCamera;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(mainCamera.transform);
     }
 }
