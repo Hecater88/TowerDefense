604ab777a5bedd3d677ac816d62c735be7caff35
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2022-01-25 17:48:17 +0100

    Enemy Movement, prefab road fixed

--- TowerDefense/Assets/Scripts/Enemy.cs
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = Waypoint.points[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+
+            GetNextWaypoint();
+    }
+
+    void GetNextWaypoint()
+    {
+        if(wavepointIndex >= Waypoint.points.Length-1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoint.points[wavepointIndex];
     }
 }
